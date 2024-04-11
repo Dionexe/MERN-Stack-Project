37b497c9e8284cb8f3f34caa44e5052c248041bf
Zeth Thomas <zeththomas89@gmail.com>
2024-04-10 20:14:03 -0700

    Mern stack layout

---
+++ index.js
@@ -0,0 +1,36 @@
+// Modules and Globals
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const router = require('express').Router()
+const methodOverride = require('method-override');
+
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'));
+
+
+app.get('/', (req, res) => {
+    res.send(render('home'));
+    console.log()
+});
+
+// Wildcard/404 route
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+});
+
+module.exports = router
